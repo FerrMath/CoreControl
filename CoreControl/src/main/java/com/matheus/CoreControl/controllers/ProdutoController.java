2d17b6c80d479b2360aad0d6b966c5764bce7d1a
Matheus A <maf_dev@hotmail.com>
2024-06-02 03:15:10 -0300

    Finish product info front-end

--- CoreControl/src/main/java/com/matheus/CoreControl/controllers/ProdutoController.java
+++ CoreControl/src/main/java/com/matheus/CoreControl/controllers/ProdutoController.java
@@ -5,25 +5,33 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/produtos")
 public class ProdutoController {
+
+    @GetMapping("/")
+    public String listProducts() {
+        return "products-list";
+    }
+
+    // TODO temporário
+    @GetMapping("/produto/{id}")
+    public String getMethodName() {
+        return "product";
+    }
 
     @GetMapping("/novo")
     public String showPordutctForm() {
         return "product-form";
     }
 
+    // TODO Redireciona para a página inicial temporariamente
     @PostMapping("/salvar")
     public String postMethodName(@RequestBody String entity) {
         System.out.println("Salvou com sucesso o produto");
-        return "redirect:/"; // Redireciona para a página inicial temporariamente
-    }
-
-    @GetMapping("/")
-    public String listProducts() {
-        return "products-list";
+        return "redirect:/";
     }
 
 }
